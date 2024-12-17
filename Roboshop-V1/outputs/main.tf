005fbc877115eb3e157415d65c3100bac4638bac
Prughvi <prudhviraj3525@gmail.com>
2024-12-17 15:03:02 +0530

    v

--- Roboshop-V1/outputs/main.tf
+++ Roboshop-V1/outputs/main.tf
@@ -1,6 +1,7 @@
 # variable "url" {
 #   default = "google.com"
 # } # Declaration of a variable
-output "URL" {
-  value = example.com
-}
+# output "URL" {
+#   value = example.com
+# }
+variable "name" {}
