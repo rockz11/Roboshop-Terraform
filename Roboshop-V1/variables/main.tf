545833a794ffc9eacbb626ab52799dcd045bd9dd
Prughvi <prudhviraj3525@gmail.com>
2024-12-17 13:24:30 +0530

    v

--- Roboshop-V1/variables/main.tf
+++ Roboshop-V1/variables/main.tf
@@ -2,5 +2,5 @@ variable "URL" {
   default = "google.com"
 }
 output "URL" {
-  value = "var.URL" # Accessing a variable.
+  value = var.URL # Accessing a variable. important Using double quotes in output's value Here terraform considers this as string ans prints as it ,instead of desired ones.
 }
