283179e4bd3875c89a611fa9b059c5aec757b36d
Prughvi <prudhviraj3525@gmail.com>
2024-12-17 16:13:38 +0530

    v

--- Roboshop-V1/loop/count.tf
+++ Roboshop-V1/loop/count.tf
@@ -1,4 +1,38 @@
+variable "instances" {
+  default = [
+    "frontend",
+    "mongodb",
+    "catalogue",
+    "cart",
+    "user",
+    "redis",
+    "mysql",
+    "shipping",
+    "payment",
+    "rabbitmq",
+    "dispatch",
 
-resource "null_resource" "demo" {
-  count = 11
+  ]
+}
+
+
+
+
+resource "aws_instance" "instance" {
+  count = length(var.instances)
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+resource "aws_route53_record" "frontend"
+  count = length(var.instances)
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "${var.instances[count.index]}-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[count.index].private_ip]
+
 }
