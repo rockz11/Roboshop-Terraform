a3e1250b145c59bb912889c529381d21d53c7f38
Prughvi <prudhviraj3525@gmail.com>
2024-12-18 13:51:20 +0530

    m

--- Roboshop-V1/data-source/main.tf
+++ Roboshop-V1/data-source/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "test" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
-  vpc_security_group_ids = [data.aws_security_group.id]
+  vpc_security_group_ids = [data.aws_instance.test.id]
   tags = {
     Name = "test"
   }
