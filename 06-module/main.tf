3664567e9709e976a060eb8afee30e31048f5b10
Prughvi <prudhviraj3525@gmail.com>
2024-12-18 15:26:15 +0530

    m

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -11,10 +11,13 @@ module "ec2" {
   source = "./ec2"
   instance_name = var.instance[count.index]
 }
-
-module "route53" {
-  count = length(var.instances)
-  source = "./route53"
-  instance_name = var.instances[count.index]
-  ip_address = module.ec2
+output "ip" {
+  value = "module.ec2"
 }
+
+# module "route53" {
+#   count = length(var.instances)
+#   source = "./route53"
+#   instance_name = var.instances[count.index]
+#   ip_address = module.ec2[count.index].ip_address
+# }
