8fd9d6c88644ec397a6141eafd11437f454b3b20
Prughvi <prudhviraj3525@gmail.com>
2024-12-18 13:41:50 +0530

    m

---
+++ Roboshop-V1/data-source/main.tf
@@ -0,0 +1,15 @@
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  tags = {
+    Name = "test"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "RHEL-9-DevOps-Practice"
+  owners = ["973714476881"]
+}
