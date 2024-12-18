7f556e015e48072a73d985de629452a6c46e3314
Prughvi <prudhviraj3525@gmail.com>
2024-12-18 15:22:05 +0530

    m

--- 06-module/ec2/main.tf
+++ 06-module/ec2/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,3 +1,11 @@
+variable "instances" {
+  default = [
+    "test1",
+    "test2",
+
+  ]
+}
+
 module "ec2" {
   count = length(var.instances)
   source = "./ec2"
